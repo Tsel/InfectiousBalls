9ab49279f2ac69f44673131a1f1f09da228b0e6f
Tsel <thomas.selhorst@online.de>
2015-08-05 13:31:02 +0200

    Circle added to screen (canvas)

--- CraeteWindow.py
+++ CraeteWindow.py
@@ -6,6 +6,9 @@ background_colour = (255, 255, 255)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tutorial 1")
 screen.fill(background_colour)
+#
+# draw a circle
+pygame.draw.circle(screen,(0,0,255,255), (150,50), 125, 1)
 pygame.display.flip()
 
 running = True
